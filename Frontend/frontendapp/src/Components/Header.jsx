6ba837cd848b690b50595c83aac944d0f17860b9
galaxyyyyyy <chenyuegalaxy@gmail.com>
2024-11-24 20:50:31 -0500

    update Header.jsx and Login.jsx for input box

--- Frontend/frontendapp/src/Components/Header.jsx
+++ Frontend/frontendapp/src/Components/Header.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
 const Header = () => {
-    
-    const navigate = useNavigate();
-    const handleLoginClick = () => {
-        navigate("/Login");
-    }
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate("/Login");
+  };
 
-    return (
+  return (
     <header style={headerStyle}>
       <div style={containerStyle}>
         <h1 style={titleStyle}>SafeTrade</h1>
         <div style={searchContainerStyle}>
           <div style={searchWrapperStyle}>
             <i className="fas fa-search" style={searchIconStyle}></i>
-            <input type="text" style={searchStyle} placeholder="Search for products..." />
+            <input
+              type="text"
+              style={searchStyle}
+              placeholder="Search for products..."
+            />
           </div>
         </div>
         <div style={buttonContainerStyle}>
           <div style={verticalLineStyle}></div> {/* Vertical line */}
-          <a href="/SignUp" style={linkStyle}>Sign Up</a>
-          <button onClick={handleLoginClick} style={buttonStyle}>Login</button>
+          <a href="/SignUp" style={linkStyle}>
+            Sign Up
+          </a>
+          <button onClick={handleLoginClick} style={buttonStyle}>
+            Login
+          </button>
         </div>
       </div>
     </header>
@@ -31,105 +36,103 @@ const Header = () => {
 };
 
 const headerStyle = {
-  background: '#007BFF',
-  textAlign: 'center',
-  padding: '10px 20px',
-  border: 'none',
-  margin: '0',
-  boxSizing: 'border-box',
-  height: '150px', // Increase the height of the header
-
+  background: "#007BFF",
+  textAlign: "center",
+  padding: "10px 20px",
+  border: "none",
+  margin: "0",
+  boxSizing: "border-box",
+  height: "150px", // Increase the height of the header
 };
 
 const titleStyle = {
-  fontFamily: 'Inter, sans-serif',
-  fontWeight: 'bold',
-  fontStyle: 'italic',
-  textShadow: '2px 4px 4px rgba(0,0,0,0.5)',
-  color: '#F0F0F0', // Lighter grey color for better visibility
-  marginLeft: '-100px', // Remove default margin
-  padding: '0' // Remove padding to adjust height
+  fontFamily: "Inter, sans-serif",
+  fontWeight: "bold",
+  fontStyle: "italic",
+  textShadow: "2px 4px 4px rgba(0,0,0,0.5)",
+  color: "#F0F0F0", // Lighter grey color for better visibility
+  //marginLeft: "-100px", // Remove default margin
+  padding: "0", // Remove padding to adjust height
 };
 
-
-
 const containerStyle = {
-  maxWidth: '1300px',
-  margin: '0 auto',
-  padding: '0', // Remove padding to adjust height
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center', // Center align items vertically
-  height: '100%' // Ensure the container takes the full height of the header
+  maxWidth: "1300px",
+  margin: "0 auto",
+  padding: "0", // Remove padding to adjust height
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center", // Center align items vertically
+  height: "100%", // Ensure the container takes the full height of the header
 };
 
 const searchContainerStyle = {
-  flex: '1',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '0' // Remove padding to adjust height
+  flex: "1",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "0", // Remove padding to adjust height
 };
 
 const searchWrapperStyle = {
-  position: 'relative',
-  width: '550px',
-  marginLeft: '20px' // Remove margin to adjust height
+  position: "relative",
+  width: "100%",
+  marginLeft: "20px", // Remove margin to adjust height
 };
 
 const searchIconStyle = {
-  position: 'absolute',
-  left: '10px',
-  top: '50%',
-  transform: 'translateY(-50%)',
-  color: '#ccc',
-  fontSize: '20px'
+  position: "absolute",
+  left: "10px",
+  top: "50%",
+  transform: "translateY(-50%)",
+  color: "#ccc",
+  fontSize: "20px",
 };
 
 const searchStyle = {
-  width: '100%',
-  padding: '10px 10px 10px 50px', // Add padding to the left to make space for the icon
-  borderRadius: '20px',
-  border: '1px solid #ccc',
-  fontFamily: 'Inter, sans-serif',
-  fontSize: '20px',
+  width: "100%",
+  padding: "10px 10px 10px 40px", // Add padding to the left to make space for the icon
+  borderRadius: "20px",
+  border: "1px solid #ccc",
+  fontFamily: "Inter, sans-serif",
+  fontSize: "20px",
+  boxSizing: "border-box", // Ensure padding doesn't overflow
 };
 
 const buttonContainerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '10px',
-  padding: '0' // Remove padding to adjust height
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  padding: "0", // Remove padding to adjust height
 };
 
 const buttonStyle = {
-  color: '#000',
-  backgroundColor: '#CAF0F8',
-  border: 'none',
-  padding: '10px 30px',
-  fontFamily: 'Inter, sans-serif',
-  fontSize: '18px',
-  fontWeight: 'bold',
-  cursor: 'pointer',
-  borderRadius: '10px',
-  textDecoration: 'none',
-  boxShadow: '2px 4px 4px rgba(0,0,0,0.5)',
-  marginLeft: '20px' // Add margin to the left of the button
+  color: "#000",
+  backgroundColor: "#CAF0F8",
+  border: "none",
+  padding: "10px 30px",
+  fontFamily: "Inter, sans-serif",
+  fontSize: "18px",
+  fontWeight: "bold",
+  cursor: "pointer",
+  borderRadius: "10px",
+  textDecoration: "none",
+  boxShadow: "2px 4px 4px rgba(0,0,0,0.5)",
+  marginLeft: "20px", // Add margin to the left of the button
 };
 
 const linkStyle = {
-  color: '#000',
-  textDecoration: 'none',
-  fontFamily: 'Inter, sans-serif',
-  fontSize: '20px',
-  fontWeight: 'bold',
-  cursor: 'pointer'
+  color: "#000",
+  textDecoration: "none",
+  fontFamily: "Inter, sans-serif",
+  fontSize: "20px",
+  fontWeight: "bold",
+  cursor: "pointer",
 };
 
 const verticalLineStyle = {
-  borderLeft: '1px solid #000', // Vertical line style
-  height: '50px', // Adjust height as needed
-  margin: '0 10px' // Adjust margin to add space between elements
+  borderLeft: "1px solid #000", // Vertical line style
+  height: "50px", // Adjust height as needed
+  margin: "0 10px", // Adjust margin to add space between elements
 };
 
 export default Header;
--- Frontend/frontendapp/src/Pages/Login.jsx
+++ Frontend/frontendapp/src/Pages/Login.jsx
@@ -1,31 +1,28 @@
-
 import React, { useState } from "react";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
-
-
 
 const Login = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("user"); 
+  const [role, setRole] = useState("user");
   const [isRegistering, setIsRegistering] = useState(false);
-  
-  const handleSubmit = async(e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (isRegistering) {
       console.log("Registering user:", username, password, role);
       const response = await axios.post(
-        "http://localhost:3001/register", 
+        "http://localhost:3001/register",
         { username, password, role },
         { withCredentials: true }
       );
     } else {
       console.log("Logging in user:", username, password);
       const response = await axios.post(
-        "http://localhost:3001/Login", 
+        "http://localhost:3001/Login",
         { username, password },
         { withCredentials: true }
       );
@@ -43,70 +40,156 @@ const Login = () => {
 
   return (
     <div>
-    <Header /> 
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#f4f4f4" }}>
-      <div style={{ backgroundColor: "#fff", padding: "20px", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", width: "300px", textAlign: "center" }}>
-        <h1 style={{ fontSize: "24px", marginBottom: "20px", color: "#333" }}>
-          {isRegistering ? "Register" : "Login"}
-        </h1>
-        <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="username" style={{ fontSize: "14px", marginBottom: "5px", textAlign: "left", color: "#555" }}>Username</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={{ padding: "10px", border: "1px solid #ccc", borderRadius: "4px", fontSize: "14px", width: "100%" }}
-              required
-            />
-          </div>
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="password" style={{ fontSize: "14px", marginBottom: "5px", textAlign: "left", color: "#555" }}>Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ padding: "10px", border: "1px solid #ccc", borderRadius: "4px", fontSize: "14px", width: "100%" }}
-              required
-            />
-          </div>
-          
-          
-          {isRegistering && (
-            <div style={{ marginBottom: "15px" }}>
-              <label htmlFor="role" style={{ fontSize: "14px", marginBottom: "5px", textAlign: "left", color: "#555" }}>Select Role</label>
-              <select
-                id="role"
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                style={{ padding: "10px", border: "1px solid #ccc", borderRadius: "4px", fontSize: "14px", width: "100%" }}
-                required
-              >
-                <option value="user">Normal User</option>
-                <option value="admin">Admin</option>
-              </select>
-            </div>
-          )}
-
-          <button type="submit" style={{ padding: "10px", backgroundColor: "#007BFF", color: "#fff", border: "none", borderRadius: "4px", cursor: "pointer", fontSize: "16px" }}>
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "#f4f4f4",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#fff",
+            padding: "20px",
+            borderRadius: "8px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            width: "300px",
+            textAlign: "center",
+          }}
+        >
+          <h1 style={{ fontSize: "24px", marginBottom: "20px", color: "#333" }}>
             {isRegistering ? "Register" : "Login"}
-          </button>
-        </form>
-        <p style={{ fontSize: "14px", color: "#555", marginTop: "15px" }}>
-          {isRegistering ? "Already have an account?" : "Don't have an account?"}
-          <span
-            onClick={toggleMode}
-            style={{ color: "#007BFF", cursor: "pointer", fontWeight: "bold" }}
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            style={{ display: "flex", flexDirection: "column" }}
           >
-            {isRegistering ? " Login" : " Register"}
-          </span>
-        </p>
+            <div style={{ marginBottom: "15px" }}>
+              <label
+                htmlFor="username"
+                style={{
+                  fontSize: "14px",
+                  marginBottom: "5px",
+                  textAlign: "left",
+                  color: "#555",
+                }}
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                style={{
+                  padding: "10px",
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  fontSize: "14px",
+                  width: "100%",
+                  boxSizing: "border-box",
+                }}
+                required
+              />
+            </div>
+            <div style={{ marginBottom: "15px" }}>
+              <label
+                htmlFor="password"
+                style={{
+                  fontSize: "14px",
+                  marginBottom: "5px",
+                  textAlign: "left",
+                  color: "#555",
+                }}
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{
+                  padding: "10px",
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  fontSize: "14px",
+                  width: "100%",
+                  boxSizing: "border-box",
+                }}
+                required
+              />
+            </div>
+
+            {isRegistering && (
+              <div style={{ marginBottom: "15px" }}>
+                <label
+                  htmlFor="role"
+                  style={{
+                    fontSize: "14px",
+                    marginBottom: "5px",
+                    textAlign: "left",
+                    color: "#555",
+                  }}
+                >
+                  Select Role
+                </label>
+                <select
+                  id="role"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                  style={{
+                    padding: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    fontSize: "14px",
+                    width: "100%",
+                  }}
+                  required
+                >
+                  <option value="user">Normal User</option>
+                  <option value="admin">Admin</option>
+                </select>
+              </div>
+            )}
+
+            <button
+              type="submit"
+              style={{
+                padding: "10px",
+                backgroundColor: "#007BFF",
+                color: "#fff",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+                fontSize: "16px",
+              }}
+            >
+              {isRegistering ? "Register" : "Login"}
+            </button>
+          </form>
+          <p style={{ fontSize: "14px", color: "#555", marginTop: "15px" }}>
+            {isRegistering
+              ? "Already have an account?"
+              : "Don't have an account?"}
+            <span
+              onClick={toggleMode}
+              style={{
+                color: "#007BFF",
+                cursor: "pointer",
+                fontWeight: "bold",
+              }}
+            >
+              {isRegistering ? " Login" : " Register"}
+            </span>
+          </p>
+        </div>
       </div>
     </div>
-    </div>
   );
-}
+};
 
 export default Login;
